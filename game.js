1fbc1aa7d33709431d333ec1aa8fc94287eabe68
Dennis Ranke <dennis.ranke@gmail.com>
2014-08-25 00:38:21 +0200

    add levels

--- game.js
+++ game.js
@@ -8,12 +8,28 @@ define(['gl-matrix-min', 'sphere', 'camera', 'level', 'player', 'collision', 'as
     var assetLoader = new AssetLoader(gl);
 
     var sphere = new Sphere(gl);
+    var levelIndex = 0;
     var levelData = new LevelData(0);
     var level = new Level(gl, levelData);
     var player = new Player(level, collision, assetLoader, levelData.startPos);
     var sky = new Sky(gl);
     
     this.update = function(timeStep, input) {
+      if(level.dead || level.finished) {
+        if(level.finished) {
+          player.win();
+        } else {
+          player.die();
+        }
+        if(player.isDone()) {
+          if(level.finished) {
+            levelIndex = (levelIndex + 1) % LevelData.numLevels;
+          }
+          levelData = new LevelData(levelIndex);
+          level = new Level(gl, levelData);
+          player = new Player(level, collision, assetLoader, levelData.startPos);
+        }
+      }
       player.update(timeStep, input, camera);
       level.update(timeStep);
       camera.update(timeStep, gl.drawingBufferWidth, gl.drawingBufferHeight, level, player);
